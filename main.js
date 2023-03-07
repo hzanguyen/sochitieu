3e03c4b4e059de37cf61dcd34ee621f16ac9a196
Vũ Hồng Sơn <ximvhs26092002@gmail.com>
2023-03-07 22:42:22 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -85,3 +85,93 @@ btnPrev.addEventListener('click', function () {
 });
 
 window.onload = displayInfo;
+
+// modal
+// open modal register  
+const regBtns = document.querySelectorAll('.js-register')
+const modal = document.querySelector('.js-show_register')
+function showModal() {
+    modal.classList.add('open')
+    modal1.classList.remove('open')
+}
+for (const regBtn of regBtns) {
+    regBtn.addEventListener('click', showModal)
+}
+
+
+// close register
+const closeBtn = document.querySelector('.js-close')
+function hideBtn(){
+    modal.classList.remove('open')
+}
+closeBtn.addEventListener('click', hideBtn)
+
+
+// close when click outward
+const authForms = document.querySelectorAll('.js-auth-form')
+modal.addEventListener('click', hideBtn)
+for (const authForm of authForms ) { 
+    authForm.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
+}
+
+// open modal login 
+const showLogins = document.querySelectorAll('.js-login')
+const modal1 = document.querySelector('.js-show_login')
+function showModalLogin() {
+    modal1.classList.add('open')
+    modal.classList.remove('open')
+}
+for (const showLogin of showLogins) {
+    showLogin.addEventListener('click', showModalLogin)
+}
+
+// close Login 
+const closeLoginBtns = document.querySelectorAll('.js-login_close')
+function hideLoginBtn(){
+    modal1.classList.remove('open')
+}
+for (const closeLoginBtn of closeLoginBtns) {
+    closeLoginBtn.addEventListener('click', hideLoginBtn)
+}
+
+// close when click outward
+const authLoginForms = document.querySelectorAll('.js-auth-form_login')
+modal1.addEventListener('click', hideLoginBtn)
+for (const authLoginForm of authLoginForms ) { 
+    authLoginForm.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
+}
+
+
+
+// info-nhóm
+const infoBtns = document.querySelectorAll('.js-info')
+const modalInfo = document.querySelector('.js-show_info')
+function showModalInfo() {
+    modalInfo.classList.add('open')
+    modal2.classList.remove('open')
+}
+for (const infoBtn of infoBtns) {
+    infoBtn.addEventListener('click', showModalInfo)
+}
+
+
+// close info
+const closeInfoBtn = document.querySelector('.js-info-close')
+function hideBtnInfo(){
+    modalInfo.classList.remove('open')
+}
+closeInfoBtn.addEventListener('click', hideBtnInfo)
+
+
+// close when click outward
+const authFormsInfo = document.querySelectorAll('.js-auth-form-info')
+modalInfo.addEventListener('click', hideBtnInfo)
+for (const authFormInfo of authFormsInfos ) { 
+    authForm.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
+}
